181595e39fb9db5e825c75403356fb02aec36f9c
unknown <manhack7897@gmail.com>
2021-03-18 22:11:46 -0400

    apiin nodejs  for registration of customers

---
+++ javascript/Registerambulance.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Ambulance-Service')
+.then(()=>console.log('Connected to Db'))
+.catch(err=>console.error('Error',err))
+
+const registerSchema= new mongoose.Schema({
+     firstname:String,
+     lastname:String,
+     email:String,
+     password:String,
+     confirmpassword:String,
+     phoneno:String,
+     Address:String
+});
+const registertionModel = mongoose.model('Registration-Ambulance',registerSchema);
+
+module.exports=registertionModel;
+async function registerPatient(){
+
+const register = new registertion({
+    firstname:'Mandeep',
+    lastname:'Singh',
+    email:'mandeep@gmail.com',
+    password:'345678',
+    confirmpassword:'345678',
+    phoneno:'4567899876',
+    Address:'1345 rue champm'
+   
+});
+
+const result = await register.save();
+console.log(result);
+
+}
+   registerPatient();
+
+
